2d4cbb3c2b23b2bb23e299d38044e5ad487e9159
Yatin <127234342+Yatin-Malhotra@users.noreply.github.com>
2023-07-20 10:51:21 -0400

    Create Product.java

---
+++ app/src/main/java/com/example/b07project/Product.java
@@ -0,0 +1,45 @@
+package com.example.b07project;
+
+public class Product {
+    private String imageURL;
+    private String title;
+    private float price;
+    private String description;
+    private int storeID;
+    private int productID;
+
+    public Product(String imageURL, String title, float price, String description, int storeID, int productID) {
+        this.imageURL = imageURL;
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.storeID = storeID;
+        this.productID = productID;
+
+    }
+
+    public String getImageURL() { return imageURL; }
+
+    public String getTitle() { return title; }
+
+    public float getPrice() { return price; }
+
+    public String getDescription() { return description; }
+
+    public int getStoreID() { return storeID; }
+
+    public int getProductID() { return productID; }
+
+    public void setImageURL(String imageURL) { this.imageURL = imageURL; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public void setPrice(float price) { this.price = price; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    public void setStoreID(int storeID) { this.storeID = storeID; }
+
+    public void setProductID(int productID) { this.productID = productID; }
+
+}
