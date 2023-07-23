18b6b188661404283eabe64b972f7a451abef665
Taha <70107774+tsids@users.noreply.github.com>
2023-07-23 11:56:14 -0400

    Added extra classes to make it easier to organize info (#5)
    * Create CartProduct.java
    
    * Created Store.java
    
    * Create Cart.java
    
    * Update CartProduct.java
    
    Removed the implementation of Serializable since the super class already implements it
    
    ---------
    
    Co-authored-by: Yatin <127234342+Yatin-Malhotra@users.noreply.github.com>

---
+++ app/src/main/java/com/example/b07project/Cart.java
@@ -0,0 +1,17 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+
+public class Cart implements Serializable {
+
+    ArrayList<CartProduct> cart = new ArrayList<>();
+
+    public Cart() {}
+
+    public void addObject(CartProduct item) { cart.add(item); }
+
+    public void removeObject(CartProduct item) { cart.remove(item); }
+
+    public ArrayList<CartProduct> getCart() { return cart; }
+
+}
