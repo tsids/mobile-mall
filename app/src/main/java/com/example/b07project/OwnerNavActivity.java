2bd0cfd5f7ff8212b979fff4a99f48084f5205d1
Taha <tsid2181@gmail.com>
2023-07-23 14:16:42 -0400

    added navbar for store owner with icons

---
+++ app/src/main/java/com/example/b07project/OwnerNavActivity.java
@@ -0,0 +1,57 @@
+package com.example.b07project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.b07project.databinding.ActivityOwnerNavBinding;
+
+public class OwnerNavActivity extends AppCompatActivity {
+
+    ActivityOwnerNavBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityOwnerNavBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new ProductsFragment());
+
+        binding.ownerNavigationView.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.products) {
+                replaceFragment(new ProductsFragment());
+            }
+            else if (item.getItemId() == R.id.orders) {
+                replaceFragment(new OrdersFragment());
+            }
+
+            return true;
+        });
+        switchToUser();
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
+    private void switchToUser() {
+        Button button = findViewById(R.id.switch_user);
+        button.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(OwnerNavActivity.this, UserNavActivity.class));
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/b07project/UserNavActivity.java
@@ -0,0 +1,61 @@
+package com.example.b07project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.b07project.databinding.ActivityUserNavBinding;
+
+public class UserNavActivity extends AppCompatActivity {
+
+    ActivityUserNavBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityUserNavBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new StoresFragment());
+
+        binding.userNavActivity.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.stores) {
+                replaceFragment(new ProductsFragment());
+            }
+            else if (item.getItemId() == R.id.cart) {
+                replaceFragment(new CartFragment());
+            }
+            else if (item.getItemId() == R.id.orders) {
+                replaceFragment(new OrdersFragment());
+            }
+
+            return true;
+        });
+        switchToOwner();
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
+    private void switchToOwner() {
+        Button button = findViewById(R.id.switch_owner);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(UserNavActivity.this, OwnerNavActivity.class));
+            }
+        });
+    }
+
+}
