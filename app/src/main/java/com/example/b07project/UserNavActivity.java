0ff1193bbb89f32a2b3f9db21999edd87eb00469
OrhanBC <orhancangurel@gmail.com>
2023-07-24 12:45:13 -0400

    changed default loading page

--- app/src/main/java/com/example/b07project/UserNavActivity.java
+++ app/src/main/java/com/example/b07project/UserNavActivity.java
@@ -5,10 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.example.b07project.databinding.ActivityUserNavBinding;
 
@@ -31,8 +28,8 @@ public class UserNavActivity extends AppCompatActivity {
             else if (item.getItemId() == R.id.cart) {
                 replaceFragment(new CartFragment());
             }
-            else if (item.getItemId() == R.id.orders) {
-                replaceFragment(new OrdersFragment());
+            else if (item.getItemId() == R.id.past_orders) {
+                replaceFragment(new PastOrdersFragment());
             }
 
             return true;
