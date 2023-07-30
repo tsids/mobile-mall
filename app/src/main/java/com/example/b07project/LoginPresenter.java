969947f5005a89606763e5b9a34eaa414c97d7a9
OrhanBC <orhancangurel@gmail.com>
2023-07-30 15:14:03 -0400

    added sign up

--- app/src/main/java/com/example/b07project/LoginPresenter.java
+++ app/src/main/java/com/example/b07project/LoginPresenter.java
@@ -1,5 +1,13 @@
 package com.example.b07project;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class LoginPresenter implements LoginContract.Presenter {
 
 
@@ -7,6 +15,7 @@ public class LoginPresenter implements LoginContract.Presenter {
     LoginContract.Model model;
     public boolean usernameExists;
     public boolean passwordMatches;
+    FirebaseDatabase db;
 
     public LoginPresenter(LoginContract.View view, LoginContract.Model model) {
         this.view = view;
@@ -91,6 +100,22 @@ public class LoginPresenter implements LoginContract.Presenter {
     @Override
     public void createAccount (String username, String userType, String password) {
         //add username, password to database and switch to user navigation
+        db = FirebaseDatabase.getInstance("https://b07project-4cc9c-default-rtdb.firebaseio.com/");
+        DatabaseReference ref= db.getReference();
+        DatabaseReference query = ref.child("users").child(username);
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                query.child("username").setValue(username);
+                query.child("password").setValue(password);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
 
