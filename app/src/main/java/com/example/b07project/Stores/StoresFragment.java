4e3ddfdac55313d2ace0a4fc23aa5d4afdcb637f
OrhanBC <orhancangurel@gmail.com>
2023-07-30 00:39:20 -0400

    added toasts to let the user know the product is added to cart or when the quantity is less than or equal to 0

--- app/src/main/java/com/example/b07project/Stores/StoresFragment.java
+++ app/src/main/java/com/example/b07project/Stores/StoresFragment.java
@@ -21,9 +21,11 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class StoresFragment extends Fragment implements RecyclerViewInterface {
